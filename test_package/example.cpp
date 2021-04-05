69c2122510a37b37d3f8ced7508fe555c4615de7
Vladimir Bayrashevskiy <umogSlayer@users.noreply.github.com>
2021-04-05 14:55:48 +0700

    Implement test for linux

--- test_package/example.cpp
+++ test_package/example.cpp
@@ -21,6 +21,18 @@ bool dumpCallback(const wchar_t* _dump_dir,
 }
 #endif
 
+#ifdef __linux__
+#include <breakpad/client/linux/handler/exception_handler.h>
+#include <breakpad/client/linux/handler/minidump_descriptor.h>
+static bool dumpCallback(
+    const google_breakpad::MinidumpDescriptor &md,
+    void *context,
+    bool succeeded)
+{
+    return false;
+}
+#endif
+
 int main(int argc, char* argv[]) {
 #ifdef __APPLE__
   std::string path = "/tmp";
@@ -30,6 +42,13 @@ int main(int argc, char* argv[]) {
 #ifdef _WIN32
   std::wstring path = L"C:\\tmp";
   google_breakpad::ExceptionHandler eh(path, 0, dumpCallback, 0, google_breakpad::ExceptionHandler::HandlerType::HANDLER_ALL);
+#endif
+
+#ifdef __linux__
+  using google_breakpad::MinidumpDescriptor;
+  using google_breakpad::ExceptionHandler;
+  MinidumpDescriptor md("/tmp");
+  ExceptionHandler eh(md, nullptr, static_cast<ExceptionHandler::MinidumpCallback>(dumpCallback), nullptr, true, -1);
 #endif
 
   return 0;
